655b4a6301df42673e3d4434c407a04f9c010096
svuggu <svuggu@dxc.com>
2021-03-23 11:50:04 +0530

    zip demo added

---
+++ zipdemo.py
@@ -0,0 +1,13 @@
+
+names = ("vasu", 'divya', 'tej', "navin", "navin")
+
+comp = ("oracle", "dxc", "school", "del", "hp")
+
+zipped = zip(names, comp)
+
+print(zipped)
+
+for (a, b) in zipped:
+    print(a, b)
+
+
